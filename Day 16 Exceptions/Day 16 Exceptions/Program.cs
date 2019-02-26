587e3d1ece149477444ffb64777f9ced6326b2f3
Khalid Hassan <khalidhsoliman@gmail.com>
2019-02-26 04:19:08 +0200

    Day 16 Exceptions

---
+++ Day 16 Exceptions/Day 16 Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+        int N = 0; 
+
+        try
+        {
+            N = int.Parse(S);
+            Console.WriteLine(S); 
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine("Bad String");
+        }
+
+        Console.ReadKey();
+    }
+}
+
