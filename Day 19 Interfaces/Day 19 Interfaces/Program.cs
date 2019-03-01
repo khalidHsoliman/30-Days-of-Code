3b0f2ffe62f82e9d5588cc12902b6ff2f9ba7d8b
Khalid Hassan <khalidhsoliman@gmail.com>
2019-03-01 23:07:33 +0200

    Day 19 Interfaces

---
+++ Day 19 Interfaces/Day 19 Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int divSum = 0;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+                divSum += i; 
+        }
+
+        return divSum; 
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+    }
+}
