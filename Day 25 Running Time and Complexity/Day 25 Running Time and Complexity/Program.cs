ff23062402a6b74636019f9c8799c1af97c5ff86
Khalid Hassan <khalidhsoliman@gmail.com>
2019-03-06 04:20:32 +0200

    Day 25 Running Time and Complexity: Prime numbers

---
+++ Day 25 Running Time and Complexity/Day 25 Running Time and Complexity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int t = int.Parse(Console.ReadLine()); 
+
+        while(t-- > 0)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(num) ? "Prime" : "Not prime");  
+        }
+
+        Console.ReadLine(); 
+    }
+
+    private static bool IsPrime(int num)
+    {
+        if (num == 1)
+            return false;
+
+        int m = num / 2; 
+
+        for (int i = 2; i <= m; i++)
+        {
+            if (num % i == 0)
+                return false;       
+        }
+
+        return true; 
+    }
+}
+
