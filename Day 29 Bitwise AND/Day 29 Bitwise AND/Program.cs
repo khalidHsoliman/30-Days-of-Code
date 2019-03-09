a447f2314c4ee4dac95d91ef1648bbd7135baab3
Khalid Hassan <khalidhsoliman@gmail.com>
2019-03-09 14:15:52 +0200

    Day 29 Bitwise AND

---
+++ Day 29 Bitwise AND/Day 29 Bitwise AND/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(nk[0]);
+            int k = Convert.ToInt32(nk[1]);
+
+            Console.WriteLine((k-1 | k) <= n ? k-1 : k-2); 
+        }
+
+        Console.ReadKey();
+    }
+}
